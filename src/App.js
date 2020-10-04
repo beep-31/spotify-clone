dad6d1444b096a96631a9a71f60aa2c1e835289b
beep-31 <alex.scheglov25@gmail.com>
2020-10-04 17:05:01 +0200

    Authorisation is finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  //Run some code based on a given condition
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+    }
+  }, []);
   return (
     <div className="app">
-      {/* spotify logo */}
-      <Login />
+      {token ? <h1>i am logged in</h1> : <Login />}
+      );
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import Logo from "./img/Spotify__logo.jpg";
+import "./Login.css";
+import { loginUrl } from "./spotify";
 function Login() {
-  return <div></div>;
+  return (
+    <div className="login">
+      <img src={Logo} alt="Spotify" className="login__logo" />
+      <a className="login__btn" href={loginUrl}>
+        Login with spotify
+      </a>
+    </div>
+  );
 }
 
 export default Login;
